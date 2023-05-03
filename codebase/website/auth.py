61c02c28593af366ddc03560b9ba2845a8661bc3
Alvin Dang <alvin.dang@yahoo.com>
2023-05-03 10:59:02 -0700

    Update auth.py

--- codebase/website/auth.py
+++ codebase/website/auth.py
@@ -65,6 +65,7 @@ def sign_up():
         password1 = request.form.get('password1')
         password2 = request.form.get('password2')
 
+
         user = User.query.filter_by(email=email).first()
 
         if not email:
