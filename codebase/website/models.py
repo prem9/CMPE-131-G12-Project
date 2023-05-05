1cd652348cd6b0567dea5bfd280cb32f2ef9d631
prem9 <prem.m.agarwal@gmail.com>
2023-05-04 22:00:25 -0700

    not finish but good work on messages

--- codebase/website/models.py
+++ codebase/website/models.py
@@ -16,3 +16,12 @@ class User(db.Model, UserMixin):
     password = db.Column(db.String(150))
     first_name = db.Column(db.String(150))
     notes = db.relationship('Note')
+
+class Message(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    sender_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
+    receiver_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
+    content = db.Column(db.String(200), nullable=False)
+
+    def __repr__(self):
+        return f'<Message {self.id}>'
