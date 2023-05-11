5b16b910ea94c984361e254d47bf70ec3907dd07
prem9 <prem.m.agarwal@gmail.com>
2023-05-10 23:36:49 -0700

    implemented delete chat and create chat, worked on accessing database

--- codebase/instance/testdb.py
+++ codebase/instance/testdb.py
@@ -1,11 +1,50 @@
 import sqlalchemy as db
+from sqlalchemy import inspect
 
-engine = db.create_engine("sqlite:///appdata.db")
-connection = engine.connect()
+engine = db.create_engine("sqlite+pysqlite:///appdata.db") #,echo=True)
+inspector = inspect(engine)
+tables = inspector.get_table_names()
+print(tables)
+
+#conn = engine.connect()
+
 metadata = db.MetaData()
-User = db.Table("User", metadata, autoload=True, autoload_with=engine)
+metadata.reflect(bind=engine)
 
 
-query = db.select([User.columns.state.distinct()])
-resutl = connection.execute(query).scalar()
-print(result)
+#conn.execute("SELECT * FROM User")
+#conn.commit()
+
+User = db.Table("user", metadata, autoload_with=engine)
+
+
+#query = db.select([User.columns.state.distinct()])a
+
+stmt = User.select()
+with engine.connect() as conn:
+    result = conn.execute(stmt)
+    rows = result.fetchall()
+
+for row in rows:
+    print(row)
+
+note = db.Table("note", metadata, autoload_with=engine)
+stmt = note.select()
+with engine.connect() as conn:
+    result = conn.execute(stmt)
+    rows = result.fetchall()
+for row in rows:
+    print(rows)
+
+message = db.Table("message",metadata, autoload_with=engine)
+stmt = message.select()
+with engine.connect() as conn:
+    result = conn.execute(stmt)
+    rows = result.fetchall()
+for row in rows:
+    print(rows)
+
+#stmt = select(*User.__table__.columns)
+#rows = connection.execute(stmt)
+#resutl = connection.execute(query).scalar()
+#print(result)
--- codebase/website/chats.py
+++ codebase/website/chats.py
@@ -3,49 +3,93 @@ from flask_login import login_required, current_user
 from .models import Message, User
 from . import db
 import json
-import sqlalchemy.sql 
+import sqlalchemy.sql
 
 
 #messages_BP = Blueprint('messages', __name__)
 chats_BP = Blueprint('chats', __name__)
 
+@chats_BP.route('/delete_chat', methods=['GET','POST'])
+@login_required
+def delete_message():
+    user_id = current_user.id
+    if request.method == "POST":
+        email2 = request.form['email']
+        user2 = User.query.filter_by(email=email2).first()
+        chat1 = Message.query.filter_by(sender_id=user_id, receiver_id=user2.id).first()
+        chat2 = Message.query.filter_by(receiver_id=user_id, sender_id=user2.id).first()
+        if not user2:
+            return return_template("delete_chat.html", user=current_user, error="invalid email")
+        if not chat1:
+            return return_template("delete_chat.html", user=current_user, error="No chat for this email")
+        db.session.delete(chat1)
+        db.session.delete(chat2)
+        db.session.commit()
+    else:
+        return render_template("delete_chat.html", user=current_user)
+
+    return render_template('delete_chat.html', user=current_user)
 @chats_BP.route('/chathome', methods=['GET','POST'])
 @login_required
 def chat_home():
-    pass
-    return render_template('chathome.html', user=current_user)
+    user_id = current_user.id
+    #html = "<table>"
+    if request.method == "GET":
+        msgs = Message.query.filter_by(sender_id=user_id).all()
+        html = ""
+        for row in msgs:
+            print(row)
+            #html += "<tr>"
+            user1 = User.query.filter_by(id=row.receiver_id).first()
+            html += str(user1.email)+str("\n")
+            '''
+            for value in row:
+                html += "<td>{}</td>".format(value)
+            '''
+            #html += "</tr><br>"
+            
+    elif request.method == "POST":
+        msgs = Message.query.filter_by(sender_id=user_id).all()
+
+        for row in msgs:
+            html += "<tr>"
+            html += str(row)
+            '''
+            for value in row:
+                html += "<td>{}</td>".format(value)
+            html += "</tr>"
+            '''
+    #html += "</table>"
+    return render_template('chathome.html', user=current_user, table=html)
 
 # Define the messages route
-@chats_BP.route('/chats', methods=['GET', 'POST'])
+@chats_BP.route('/chat', methods=['GET', 'POST'])
 @login_required
 def chats():
 
     user_id = current_user.id #session['user_id']
-    user = User.query.filter_by(id=user_id).first()
+    #user = User.query.filter_by(id=user_id).first()
 
     if request.method == 'POST':
         #db.select('user')
-        #sql_str = text("select * from Message")
-        #db.session.execute(sql_str).fetchall()a
-        query = db.session.query(Message).filter_by(id=user_id)
-        for cols in db.session.connection().execute(query):
-            print(cols)
-
-        receiver_username = request.form['receiver']
-        receiver = User.query.filter_by(username=receiver_username).first()
+	#line 28 error on 29
+        receiver_username = request.form['email']
+        receiver = User.query.filter_by(email=receiver_username).first()
         if not receiver:
-            return render_template('chat.html', user=user, error='Invalid username')
+            return render_template('chat.html', user=current_user, error='Invalid username')
 
-        content = request.form['content']
-        message = Message(sender_id=user_id, receiver_id=receiver.id, content=content)
-        db.session.add(message)
+        content = request.form['message']
+        message1 = Message(sender_id=user_id, receiver_id=receiver.id, content=content)
+        message2 = Message(sender_id=receiver.id, receiver_id=user_id, content=content)
+        db.session.add(message1)
+        db.session.add(message2)
         db.session.commit()
     else:
-        return render_template('chat.html', user=user)
+        return render_template('chat.html', user=current_user)
 
     messages_sent = Message.query.filter_by(sender_id=user_id).all()
     messages_received = Message.query.filter_by(receiver_id=user_id).all()
-    return render_template('chat.html')
+    return render_template('chat.html',user=current_user)
     #return render_template('messages.html', user=user, messages_sent=messages_sent, messages_received=messages_received)
 
 
